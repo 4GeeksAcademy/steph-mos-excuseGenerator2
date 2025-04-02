13146c6d1579a45d724dfab3da99809410a3ff83
steph-mos <stephymosquera@gmail.com>
2025-04-02 19:09:30 +0000

    [He creado el generador de excusas usando una función de declaración]

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  function ExcuseGenerator(excuse1){
+    
+    return excuse1[Math.floor(Math.random() * who.length)]
+  
+  }
+
+
+    let who = ['The dog ', 'My grandma ', 'The mailman ', 'My bird ','My dad '];
+    let excuseWho = ExcuseGenerator(who);
+    let action = ['ate ', 'lost ', 'crushed ', 'broke ', 'close '];
+    let excuseAction = ExcuseGenerator(action);
+    let what = ['my homework ', 'my phone ', 'the car ','the house ','my bedroom '];
+    let excuseWhat = ExcuseGenerator(what);
+    let when = ['before the class', 'while she was at the church', 'while I was exercising', 'during my lunch', 'while I was praying'];
+    let excuseWhen = ExcuseGenerator(when);
+
+
+  
+   document.getElementById("excuse").innerHTML = excuseWho + excuseAction + excuseWhat + excuseWhen;
+   
 };

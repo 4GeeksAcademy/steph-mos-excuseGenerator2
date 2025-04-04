17d610ed35df0338e9274232473eecf8475da405
steph-mos <stephymosquera@gmail.com>
2025-04-04 18:33:16 +0000

    Corrección Proyecto en grupo

--- src/app.js
+++ src/app.js
@@ -8,25 +8,25 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
 
-  function ExcuseGenerator(excuse1){
+  function randomArrayElement(array){
     
-    return excuse1[Math.floor(Math.random() * who.length)]
+    return array[Math.floor(Math.random() * array.length)]
   
   }
 
 
-    let who = ['The dog ', 'My grandma ', 'The mailman ', 'My bird ','My dad '];
-    let excuseWho = ExcuseGenerator(who);
-    let action = ['ate ', 'lost ', 'crushed ', 'broke ', 'close '];
-    let excuseAction = ExcuseGenerator(action);
-    let what = ['my homework ', 'my phone ', 'the car ','the house ','my bedroom '];
-    let excuseWhat = ExcuseGenerator(what);
+    let who = ['The dog', 'My grandma', 'The mailman', 'My bird','My dad'];
+    let excuseWho = randomArrayElement(who);
+    let action = ['ate', 'lost', 'crushed', 'broke', 'close'];
+    let excuseAction = randomArrayElement(action);
+    let what = ['my homework', 'my phone', 'the car','the house','my bedroom'];
+    let excuseWhat = randomArrayElement(what);
     let when = ['before the class', 'while she was at the church', 'while I was exercising', 'during my lunch', 'while I was praying'];
-    let excuseWhen = ExcuseGenerator(when);
+    let excuseWhen = randomArrayElement(when);
 
 
   
     
-   document.getElementById("excuse").innerHTML = excuseWho + excuseAction + excuseWhat + excuseWhen;
+   document.getElementById("excuse").innerHTML = excuseWho + " " + excuseAction+ " " + excuseWhat+ " " + excuseWhen;
    
 };
